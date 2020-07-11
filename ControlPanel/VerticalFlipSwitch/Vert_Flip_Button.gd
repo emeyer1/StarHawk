7da59c58e3c2f4b5404f045229b53a42a9bcc805
Sam <sam.mlederer@gmail.com>
2020-07-11 11:54:57 -0400

    Added Flip Switch, removed Space from controls

--- ControlPanel/VerticalFlipSwitch/Vert_Flip_Button.gd
+++ ControlPanel/VerticalFlipSwitch/Vert_Flip_Button.gd
@@ -2,17 +2,24 @@ extends Control
 
 const SHADER = preload('res://ControlPanel/OutlineSprite.shader')
 
-export(String) var TopSignal
-export(String) var BottomSignal
+export(String) var Signal
+var Button_signal = null
+
 
 func _ready():
 	material = ShaderMaterial.new()
 
 	#$TopSprite.Signal = TopSignal
-	#$BottomSprite.Signal = BottomSignal
 
 func _on_Control_mouse_entered():
 	material.shader = SHADER
 
 func _on_Control_mouse_exited():
 	material.shader = null
+
+
+func _on_Control_toggled(button_pressed):
+	if button_pressed == true:
+		Button_signal = Signal
+	else:
+		Button_signal = null
