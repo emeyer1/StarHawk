b8cf03b37d86d7f383595840dff623c4e3bedd53
Edwin <edwinmeyer47@gmail.com>
2020-07-11 15:48:44 -0400

    Added cubes and a start screen

--- Scripts/Camera.gd
+++ Scripts/Camera.gd
@@ -5,7 +5,10 @@ export var follow_speed = 2
 
 var x_bounds = 3
 var top_bound = 10
-var bottom_bound = 5
+var bottom_bound = 4
+var ship_class = preload("Ship.gd")
+
+var last_obstructing_objects = []
 
 func _ready():
 	var overlay = load("res://DebugOverlay/DebugOverlay.tscn").instance()
@@ -14,5 +17,4 @@ func _ready():
 func _physics_process(delta):
 	transform.origin.z = get_node("../Player").transform.origin.z + follow_distance
 	transform.origin.x = clamp(lerp(transform.origin.x, get_node("../Player").transform.origin.x, delta * follow_speed), -x_bounds, x_bounds)
-	transform.origin.y = clamp(lerp(transform.origin.y, get_node("../Player").transform.origin.y + 7, delta * follow_speed), bottom_bound, top_bound)
-
+	transform.origin.y = clamp(lerp(transform.origin.y, get_node("../Player").transform.origin.y + 5, delta * follow_speed), bottom_bound, top_bound)
