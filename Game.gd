2a7e0af0eac30e7760884fb234d1b91eb948f371
Sam <sam.mlederer@gmail.com>
2020-07-12 10:16:47 -0400

    GameOverState and Buttons

--- Game.gd
+++ Game.gd
@@ -11,9 +11,15 @@ func _ready():
 	ScoreDisplay.update_score(Score)
 	$AudioStreamPlayer.play()
 
+
 func ChangeScore(diff):
 	Score = Score - diff
 	ScoreDisplay.update_score(Score)
+	if Score <=0:
+		get_tree().paused = true
+		var overlay = load("res://GameOver/GameOver.tscn").instance()
+		add_child(overlay)
+		get_node("/root/").get_child(0).get_node("Camera/Controls").queue_free()
 
 
 func _on_Area_body_entered(body):
---
+++ GameOver/GameOver.gd
@@ -0,0 +1,38 @@
+extends Control
+
+onready var Game = get_node('/root/StarFax')
+
+export var level_select_scene = "DEFAULT VALUE"
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var once = false
+
+#export var next_scene = "DEFAULT VALUE"
+#export var score = 2000
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Label.text = 'Better Luck Next Time'
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_TextureButton_toggled(button_pressed):
+	if !once:
+		once = true
+		restart()
+	
+func restart():
+	yield(get_tree().create_timer(0.25), "timeout")
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+
+func _on_LevelSelect_toggled(button_pressed):
+	if !once:
+		once = true
+		get_tree().change_scene(level_select_scene)
